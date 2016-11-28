6f704c5c0d132cc427188c93228bf7fe5a58e850
Santeri J Remes <Santeri.Remes@edu.savonia.fi>
2016-11-28 14:43:49 +0200

    Level4 jatkettu

---
+++ Assets/Scripts/FreezeRotation.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FreezeRotation : MonoBehaviour {
+
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        if (rb2d != null)
+        {
+            rb2d.freezeRotation = true;
+        }
+	}
+}
---
+++ Assets/Scripts/StopSliding.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StopSliding : MonoBehaviour {
+
+    private float speed;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+        void OnCollisionStay2D(Collision2D coll) {
+            // Jos pelaaja katsoo vasemmalle, tarvitsee kertoa -1.
+            if (coll.gameObject.tag == "Player")
+            {
+                Player player = coll.gameObject.GetComponent<Player>();
+
+                if (player.facingRight)
+                {
+                    player.transform.Translate(speed * Time.deltaTime, 0, 0);
+                }
+
+                else
+                {
+                    player.transform.Translate((speed * Time.deltaTime) * -1, 0, 0);
+                }
+            }
+            else
+            {
+                coll.gameObject.transform.Translate(speed * Time.deltaTime, 0, 0);
+            }
+        }
+}
